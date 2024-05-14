5b4b7236cdbaa03bf70895a2be89003e1d821e71
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-05-14 18:09:37 -0300

    Aulas de vue

--- pets/src/views/HomeView.vue
+++ pets/src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <span>
+    Home
+  </span>
+  <v-card title="teste">123</v-card>
 </template>
+
+<script>
+
+</script>
